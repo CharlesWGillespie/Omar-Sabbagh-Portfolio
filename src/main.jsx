969de834863fcb1f8ce46e0641cb4e474def5f7d
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-03-13 13:44:05 -0400

    added pages now working on navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css'
 
+import App from './App.jsx'
+import HomePage from './pages/HomePage.jsx';
+import ContactPage from './pages/ContactPage.jsx'
+import ResumePage from './pages/ResumePage.jsx'
+import AboutPage from './pages/AboutPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+
+      {
+        path: 'true',
+        element: <HomePage />
+      },
+      {
+        path: '/about',
+        element: <AboutPage />
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />
+      }
+    ]
+  
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
